9e3723995b53f423b97f380d6506e88882480ac1
vova-nikon <60705589+vova-nikon@users.noreply.github.com>
2020-05-03 12:56:25 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from bottle import route
 from bottle import run
+from bottle import route
 from bottle import HTTPError
 from bottle import request
 
@@ -9,35 +9,35 @@ import album
 def albums(artist):
     albums_list = album.find(artist)
     if not albums_list:
-        message = "No albums of the artist {} have been found".format(artist)
-        result = HTTPError(400, message)
+        message = "No albums of {} have been found".format(artist)
+        result = HTTPError(404, message)
     else:
         album_names = [album.album for album in albums_list]
-        result = "The artist {} has {} albums: <br>".format(artist, len(albums_list))
+        result = "There are {} albums of {}:<br>".format(len(album_names), artist)
         result += "<br>".join(album_names)
     return result
 
 @route("/albums", method="POST")
-def save_album():
+def create_album():
     year = request.forms.get("year")
     artist = request.forms.get("artist")
     genre = request.forms.get("genre")
-    album = request.forms.get("album")
+    album_name = request.forms.get("album")
 
     try:
         year = int(year)
-    except ValueError:
-        return HTTPError(400, "Incorrect year")
+    except:
+        return HTTPError(400, "The year is incorrect")
 
     try:
-        new_album = album.save_new_album(year, artist, genre, album_name)
-    except AssertionError as error:
-        result = HTTPError(400, str(error))
-    except album.AlreadyInDB as error:
-        result = HTTPError(409, str(error))
+        new_album = album.save_new(year, artist, genre, album_name)
+    except AssertionError as err:
+        result = HTTPError(400, str(err))
+    except album.AlreadyInDB as err:
+        result = HTTPError(409, str(err))
     else:
-        print("New album has been successfully saved by the id {}".format(new_album.id))
-        result = "Album {} has been successfully saved by the id {}".format(new_album.album, new_album.id)
+        print("The album {} has successfully been saved by the id {}".format(new_album.album, new_album.id))
+        result = "The album {} has successfullybeen saved by the id {}".format(new_album.album, new_album.id)
     return result
 
 if __name__ == "__main__":
